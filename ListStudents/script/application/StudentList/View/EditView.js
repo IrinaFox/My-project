957a75cea27f912d6f10c259a409e0041873816d
Liss <irina.fox88@gmail.com>
2017-03-10 01:30:49 +0200

    EditView is ready

--- ListStudents/script/application/StudentList/View/EditView.js
+++ ListStudents/script/application/StudentList/View/EditView.js
@@ -1,82 +1,83 @@
 'use strict';
 
 //Make new window, where the user can edit all data except age and birthday
-function EditView () {
-    var containerDiv = document.createElement('div'),
-        closeButton,
-        saveButton,
-        student,
-        studentJSON,
-        key;
+var EditView = (function () {
+    function EditView () {
+        this.render = function (_student) {
+            var infoWindowList = document.querySelector('#infoWindowList'),
+                containerDiv = this.createEdit(_student);
 
-    containerDiv.setAttribute('id', 'infoWindowList');
+            containerDiv.setAttribute('id', 'infoWindowList');
 
-    this.render = function (_student) {
-        var infoWindowList = document.querySelector('#infoWindowList');
+            return containerDiv;
+        };
 
-        student = _student;
-        studentJSON = student.toJSON();
+        return this;
+    }
 
-        showEdit();
-        addEventButtonClose();
-        addEventButtonSave();
-
-        return containerDiv;
-    };
-
-    function showEdit () {
-        var string = '';
+    EditView.prototype.createEdit = function (_student) {
+        var containerDiv = document.createElement('div'),
+            student = _student,
+            studentJSON = student.toJSON(),
+            string = '',
+            closeButton,
+            saveButton;
 
         delete studentJSON['birthdayDate'];
         delete studentJSON['fullName'];
         delete studentJSON['age'];
 
-        for (key in studentJSON) {
+        for (let key in studentJSON) {
             string += editTpl.replace(':value', key)
                 .replace(':key', studentJSON[key]);
         }
 
         string += buttonTpl;
         containerDiv.innerHTML = string;
-    }
 
-    function addEventButtonClose () {
-        closeButton = containerDiv.querySelector('.buttonClose');
-        closeButton.addEventListener('click', closeEditForm, false);
-    }
+        addEventButtonClose();
+        addEventButtonSave();
 
-    function closeEditForm () {
-        containerDiv.parentNode.removeChild(containerDiv);
-        closeButton.removeEventListener('click', closeEditForm);
-    }
-
-    function addEventButtonSave () {
-        saveButton = containerDiv.querySelector('.buttonSave');
-        saveButton.addEventListener('click', saveEditForm, false);
-    }
-
-    function saveEditForm () {
-        var newElements = containerDiv.querySelectorAll('.editElement'),
-            newValues = [],
-            index = 0,
-            i;
-
-        for (i = 0; i < newElements.length; i++) {
-            newValues[index] = newElements[i].value;
-            index++;
+        function addEventButtonClose () {
+            closeButton = containerDiv.querySelector('.buttonClose');
+            closeButton.addEventListener('click', closeEditForm, false);
         }
 
-        index = 0;
-
-        for (key in studentJSON) {
-            student.set(key, newValues[index]);
-            index++;
+        function closeEditForm () {
+            containerDiv.parentNode.removeChild(containerDiv);
+            closeButton.removeEventListener('click', closeEditForm);
         }
 
-        closeEditForm();
-        mediator.pub('StudentListInfoChanged', student);
-        saveButton.removeEventListener('click', saveEditForm);
-    }
+        function addEventButtonSave () {
+            saveButton = containerDiv.querySelector('.buttonSave');
+            saveButton.addEventListener('click', saveEditForm, false);
+        }
 
-    return this;
-}
+        function saveEditForm () {
+            var newElements = containerDiv.querySelectorAll('.editElement'),
+                newValues = [],
+                index = 0,
+                i;
+
+            for (i = 0; i < newElements.length; i++) {
+                newValues[index] = newElements[i].value;
+                index++;
+            }
+
+            index = 0;
+
+            for (let key in studentJSON) {
+                student.set(key, newValues[index]);
+                index++;
+            }
+
+            closeEditForm();
+            mediator.pub('StudentListInfoChanged', student);
+            saveButton.removeEventListener('click', saveEditForm);
+        }
+
+        return containerDiv;
+    };
+
+    return EditView;
+})();
